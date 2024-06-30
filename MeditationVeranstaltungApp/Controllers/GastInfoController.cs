dc29717373a730373c37819ce0aa64650296410b
Diler Singh <diler.singh83@gmail.com>
2024-06-30 23:40:24 +0200

    Gast Info List added

---
+++ MeditationVeranstaltungApp/Controllers/GastInfoController.cs
@@ -0,0 +1,23 @@
+﻿using MeditationVeranstaltungApp.Data;
+using MeditationVeranstaltungApp.Data.Migrations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeditationVeranstaltungApp.Controllers
+{
+    public class GastInfoController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public GastInfoController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var gastInfos = context.GastInfos.Include(q => q.Kontakt).ToList();
+            ViewBag.GastInfos = gastInfos;
+            return View();
+        }
+    }
+}
