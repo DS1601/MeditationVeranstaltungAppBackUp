91ae3a64b8fa978dfb83669c72a526004ae7b264
Diler Singh <diler.singh83@gmail.com>
2024-07-02 00:05:50 +0200

    New Gast Info saved

--- MeditationVeranstaltungApp/Controllers/GastInfoController.cs
+++ MeditationVeranstaltungApp/Controllers/GastInfoController.cs
@@ -1,7 +1,9 @@
 ﻿using MeditationVeranstaltungApp.Data;
 using MeditationVeranstaltungApp.Data.Migrations;
+using MeditationVeranstaltungApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace MeditationVeranstaltungApp.Controllers
 {
@@ -19,5 +21,72 @@ namespace MeditationVeranstaltungApp.Controllers
             ViewBag.GastInfos = gastInfos;
             return View();
         }
+
+        public IActionResult CreateEditGastInfo(int id)
+        {
+
+            return View();
+        }
+
+        public IActionResult CreateEditGastInfoSubmit(GastInfoModel gastInfoModel)
+        {
+            if (gastInfoModel != null)
+            {
+                var gastInfo = new GastInfo
+                {
+                    Veranstalltung = gastInfoModel.Veranstalltung,
+                    AnzahlMaenner = gastInfoModel.AnzahlMaenner,
+                    AnzahlWeiblich = gastInfoModel.AnzahlWeiblich,
+                    AnkunftAm = gastInfoModel.AnkunftAm.ToDateTime(gastInfoModel.AnkunftUm),
+                    AnkunftOrt = gastInfoModel.AnkunftOrt,
+                    AbfahrtAm = gastInfoModel.AbfahrtAm.ToDateTime(gastInfoModel.AbfahrtUm),
+                    AbfahrtOrt = gastInfoModel.AbfahrtOrt,
+                    Notiz = gastInfoModel.Notiz,
+                    UserId= "93df0a45-7232-45e6-b882-8dcc0966ba8a"
+                };
+
+
+
+                if (gastInfoModel.Id != null)
+                { //  Create
+
+                    var kontakt = context.Kontakts
+                        .Where(k => k.Vorname == gastInfoModel.Vorname &&
+                        k.Nachname == gastInfoModel.Nachname &&
+                        k.Telefon == gastInfoModel.Telefon &&
+                        k.Stadt == gastInfoModel.Stadt &&
+                        k.Land == gastInfoModel.Land
+                        )
+                        .FirstOrDefault();
+
+                    if (kontakt != null)
+                    {
+                        gastInfo.KontaktId = kontakt.Id;
+                    }
+                    else {
+                        gastInfo.Kontakt = new Kontakt
+                        {
+                            Anrede = gastInfoModel.Anrede,
+                            Vorname = gastInfoModel.Vorname,
+                            Nachname = gastInfoModel.Nachname,
+                            Geschlecht = gastInfoModel.Geschlecht,
+                            Email = gastInfoModel.Email,
+                            Telefon = gastInfoModel.Telefon,
+                            Stadt = gastInfoModel.Stadt,
+                            Land = gastInfoModel.Land,
+                        };
+                       
+                    }
+
+                    context.Add( gastInfo );
+                    context.SaveChanges();
+
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
---
+++ MeditationVeranstaltungApp/Models/GastInfoModel.cs
@@ -0,0 +1,36 @@
+﻿using MeditationVeranstaltungApp.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace MeditationVeranstaltungApp.Models
+{
+    public partial class GastInfoModel
+    {
+        public GastInfoModel()
+        {
+           
+        }
+        public int Id { get; set; }
+        public string Veranstalltung = "SAS2024HH";
+        public int AnzahlMaenner { get; set; }
+        public int AnzahlWeiblich { get; set; }
+        public DateOnly AnkunftAm { get; set; }
+        public TimeOnly AnkunftUm { get; set; }
+        public string AnkunftOrt { get; set; }
+        public DateOnly AbfahrtAm { get; set; }
+        public TimeOnly AbfahrtUm { get; set; }
+        public string AbfahrtOrt { get; set; }
+        public string? Notiz {  get; set; }
+
+        public Anrede Anrede { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public Geschlecht Geschlecht { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public string Stadt { get; set; }
+        public string Land { get; set; }
+        public int? KontaktId { get; set; }
+
+    }
+}
